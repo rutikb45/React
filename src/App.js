c3d6cfb0178e563fcc221fe43f4ba0f09cae14d5
RUTIKBHOSALE9021 <rutikbhosale7218@gmail.com>
2024-01-04 18:57:03 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
+import {useState} from 'react'
 
 function App() {
+  let [counter, setCounter] = useState(0);
   return (<>
-  <h1>Hello World</h1>
-  <h2>this is new react</h2>
-  <h3>Hello World</h3>
+ <h1>Counter</h1>
+  <h1>{counter}</h1>
+  <button onClick={()=>setCounter(counter+1)}>Increase</button>
+  <button onClick={()=>setCounter(counter-1)}>Decrease</button>
+  <button onClick={()=>setCounter(0)}>Set 0</button>
   </>
   );
 }
